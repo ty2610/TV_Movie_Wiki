daf1f3caf166e8fbbce4c7ded2835455949bb004
tyler.green <tyler.green@reeher.com>
2018-04-18 09:20:12 -0500

    added dynamic DB calls

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,17 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('/Users/tyler.green/Desktop/imdb.sqlite3');
+var dbURL;
+if (process.env.NODE_ENV === 'tyler') {
+    dbURL = "/Users/tyler.green/Desktop/imdb.sqlite3";
+} else if (process.env.NODE_ENV === 'nick') {
+    //put path to local DB here
+    dbURL = "";
+} else if (process.env.NODE_ENV === 'production') {
+    dbURL = "/../imdb.sqlite3";
+}
+
+var db = new sqlite3.Database(dbURL);
 
 /* template/test route */
 router.get('/', function(req, res, next) {
