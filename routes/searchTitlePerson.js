03d4af3daff20757efea80f2a5e740188e52afec
tyler.green <tyler.green@reeher.com>
2018-04-25 11:08:14 -0500

    fixed queries a bit

--- routes/searchTitlePerson.js
+++ routes/searchTitlePerson.js
@@ -2,25 +2,23 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
-var dbURL;
-//var bodyParser = require('body-parser');
 
-if (process.env.NODE_ENV === 'tyler') {
-    dbURL = "/Users/tyler.green/Desktop/imdb.sqlite3";
-} else if (process.env.NODE_ENV === 'nick') {
-    //put path to local DB here
-    dbURL = "";
-} else if (process.env.NODE_ENV === 'production') {
-    dbURL = "/../imdb.sqlite3";
-}
-
-var db = new sqlite3.Database(dbURL);
-
-/* template/test route */
 router.post('/', function(req, res, next) {
-    //res.render('index', { title: 'Express' });
+    var dbURL;
+
+    if (process.env.NODE_ENV === 'tyler') {
+        dbURL = "/Users/tyler.green/Desktop/imdb.sqlite3";
+    } else if (process.env.NODE_ENV === 'nick') {
+        //put path to local DB here
+        dbURL = "";
+    } else if (process.env.NODE_ENV === 'production') {
+        dbURL = "/../imdb.sqlite3";
+    }
+    var db = new sqlite3.Database(dbURL);
+
     var category = req.body.category;
     var searchText = req.body.searchText;
+
     searchText = searchText.replace(/[\[\]();]/g, "");
     searchText = searchText.replace(/[*]/g, "%");
 
@@ -32,6 +30,7 @@ router.post('/', function(req, res, next) {
     } else {
         res.send("Incorrect category provided.");
     }
+
     db.all(query, (err, result) => {
         res.send(JSON.stringify(result));
         db.close();
