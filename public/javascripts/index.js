c7559d8b2a88dbc3f6cbfd618ddb9b9ecac83c63
tyler.green <tyler.green@reeher.com>
2018-04-25 19:46:59 -0500

    better ng-repeat. put in nav bar. rows are now a link. searching can be done on the results page as well now.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,6 @@
 var indexApp = angular.module('indexApp', []);
 
 indexApp.controller('IndexController', function IndexController($scope) {
-    $scope.alert = (input) => {
-        window.alert(input);
-    };
-
     $scope.submitSearch = () => {
         var category = $("#categorySelect").val();
         var searchText = $("#searchBox").val();
@@ -13,22 +9,6 @@ indexApp.controller('IndexController', function IndexController($scope) {
         } else if("Select A Value" === category){
             window.alert("You must select a category to search for");
         } else {
-            /*var url = "/resultPage";
-            var send = {category: category, searchText: searchText};
-            $.ajax({
-                url: url,
-                data: send,
-                type: 'GET',
-                cache: false,
-                contentType: "application/json; charset=utf-8",
-                success: function (data) {
-                    var newData = JSON.parse(data);
-                    var block = 0;
-                },
-                error: function (error) {
-                    alert(error);
-                }
-            });*/
             window.location.href = "/resultPage?category=" + category + "&searchText="+searchText
         }
     };
--- public/javascripts/resultPage.js
+++ public/javascripts/resultPage.js
@@ -1,14 +1,60 @@
+$(document).ready(function() {
+    $('body').hide();
+    $(window).on('load', function() {
+        $('body').show();
+    });
+});
 var resultApp = angular.module('resultApp', []).config(['$locationProvider', function($locationProvider) { $locationProvider.html5Mode({ enabled: true, requireBase: false }); }]);
 
 resultApp.controller('ResultController', function ResultController($scope, $location) {
     $scope.searchJSON;
     $scope.isPeople;
 
-    $scope.search = () => {
-        var paramCategory = $location.search().category;
-        var paramSearchText = $location.search().searchText;
+    $scope.submitSearch = () => {
+        var category = $("#categorySelect").val();
+        var searchText = $("#searchBox").val();
+        if ("" === searchText) {
+            window.alert("You must include text to search for");
+        } else if("Select A Value" === category){
+            window.alert("You must select a category to search for");
+        } else {
+            $scope.search(category, searchText);
+        }
+    };
+
+    $scope.search = (cat,text) => {
+        var paramCategory;
+        var paramSearchText;
+        if(cat!==undefined) {
+            paramCategory = cat;
+            paramSearchText = text;
+        } else {
+            paramCategory = $location.search().category;
+            paramSearchText = $location.search().searchText;
+        }
+
+        if ("" === paramSearchText) {
+            window.alert("You must include text to search for");
+            return;
+        } else if(paramCategory !== "People" && paramCategory !== "Title"){
+            window.alert("You must select a correct category to search for");
+            return;
+        }
+
+        if(paramCategory === "People") {
+            $('#categorySelect option[value=People]').prop('selected', true);
+        } else {
+            $('#categorySelect option[value=Title]').prop('selected', true);
+        }
+
+        $('#searchBox').val(paramSearchText);
         var url = "/searchTitlePerson";
         var send = {category: paramCategory, searchText: paramSearchText};
+        if(paramCategory === "People") {
+            $scope.isPeople = true;
+        } else {
+            $scope.isPeople = false;
+        }
         $.ajax({
             url: url,
             data: send,
@@ -16,11 +62,6 @@ resultApp.controller('ResultController', function ResultController($scope, $loca
             cache: false,
             contentType: "application/x-www-form-urlencoded",
             success: (data) => {
-                if(paramCategory === "People") {
-                    $scope.isPeople = true;
-                } else {
-                    $scope.isPeople = false;
-                }
                 $scope.searchJSON = JSON.parse(data);
                 $scope.$apply();
             },
